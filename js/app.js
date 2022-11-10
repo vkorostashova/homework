3023257531caecfa09836d22f2f40f9ce1471e3a
vkorostashova <viktoria7710@gmail.com>
2022-11-11 00:22:50 +0200

    HW-3

--- js/app.js
+++ js/app.js
@@ -1,15 +1,57 @@
-function renderCountries(countries) {
+window.currencies = []
+
+function filterCurrencies(searchValue){
+   var result = [];
+   for(var currency of currencies) {
+    var currencyName = currency.txt.trim().toLowerCase();
+    var currencyValue = currency.rate + '';
+    if(currencyName.indexOf(searchValue)>=0  || currencyValue.indexOf(searchValue)>=0){
+      result.push(currency);
+    }
+   }
+   renderCurrencies(result)
+}
+
+function renderCurrencies(currencies) {
 var htmlString = '';
 
-for(var country of countries) {
+if(!currencies.length) {
+  htmlStr = `<tr><td colspan="4" class="text-center">No Items Found</td></tr>`;
+  document.getElementById('currencies').innerHTML = htmlStr;
+  return;
+}
+
+for(var currency of currencies) {
     htmlString += `<tr>
-      <td>${country.name.official}</td>
-      <td>${country.region}</td>
-      <td>${country.population}</td>
-      <td class ="text-center"><img class = "w-25" src='${country.flags.png}'/></td>
+      <td>${currency.txt}</td>
+      <td>${currency.rate}</td>
   </tr>`;
   }
   
-  document.getElementById('countries').innerHTML = htmlString;
+  document.getElementById('currencies').innerHTML = htmlString;
+
+  var trs = document.getElementsByTagName('tr');
+ for(var i=0; i<trs.length; i++) { 
+  var tr = trs[i];
+  tr.onmouseenter = function(e){
+   e.currentTarget.classList.add('bg-success')
+  }
+  tr.onmouseleave = function(e){
+    e.currentTarget.classList.remove('bg-success')
+  }
+ }
+
 }
- renderCountries(countries)
+
+fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20221110&json').then(res => res.json()).then(function(data) {
+  window.currencies = data;
+  renderCurrencies(data);
+});
+
+var search = document.getElementById('search');
+search.onkeyup = function(e) {
+  var searchValue = e.currentTarget.value;
+  filterCurrencies(searchValue.trim().toLowerCase());
+ }
+
+ 
